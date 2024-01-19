e6b82bc7c6c7d65acee6a5de76c6d004b898da29
Phillip Hall <talvor@me.com>
2024-01-20 00:39:41 +1100

    Update nord theme

--- neovim/.config/nvim/lua/user/plugins/community.lua
+++ neovim/.config/nvim/lua/user/plugins/community.lua
@@ -6,5 +6,6 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.colorscheme.nordic-nvim" },
+  
+  { import = "astrocommunity.colorscheme.nord-nvim" },
 }
